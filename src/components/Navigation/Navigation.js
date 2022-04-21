83eb3c20819cd8da44c2ca4d90cf73c344ffe866
PenKevin <kevin.pen@kpnmail.nl>
2022-04-21 17:46:45 +0200

    Navbar design and first start at page design

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+
+function Navigation({ isAuth, toggleAuth }) {
+    const history = useHistory();
+
+    function signOut() {
+        toggleAuth(false);
+        history.push('/')
+    }
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/">
+                        Home
+                    </NavLink>
+                </li>
+                {isAuth === true
+                    ? <>
+                        <li>
+                            <NavLink to="/blogposts">
+                                Blogposts
+                            </NavLink>
+                        </li>
+                        <li>
+                            <button type="button" onClick={signOut}>
+                                Uitloggen
+                            </button>
+                        </li>
+                    </>
+                    :
+                    <li>
+                        <NavLink to="/login">
+                            Login
+                        </NavLink>
+                    </li>}
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
